c1d2b7c3ed1b83b5d5f4d95600c0da36980ffdbf
Patrick Weber <pweber@spatialbit.com>
2014-11-19 11:04:54 +0100

    initial commit, basic bash and install script

---
+++ install.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+import os, fnmatch, shutil
+
+exclude = [ '*.sw*', '.git', 'install.*', '.gitmodules' ]
+
+for f in os.listdir('.'):
+    if not any(fnmatch.fnmatch(f, p) for p in exclude):
+        path = os.path.join(os.path.expanduser('~'), f)
+        if os.path.isfile(path) or os.path.isdir(path):
+            os.unlink(path)
+        os.symlink(os.path.abspath(f), path)
+        print 'create link for %s' % (path)
+
+# to get better colors in the terminal, launch the following command
+# gconftool-2 --type string --set /apps/gnome-terminal/profiles/Default/palette "#000000000000:#E5E522222222:#A6A6E3E32D2D:#FCFC95951E1E:#C4C48D8DFFFF:#FAFA25257373:#6767D9D9F0F0:#F2F2F2F2F2F2:#4CCC4CCC4CCC:#E5E522222222:#A6A6E3E32D2D:#FCFC95951E1E:#C4C48D8DFFFF:#FAFA25257373:#6767D9D9F0F0:#F2F2F2F2F2F2" 
+
